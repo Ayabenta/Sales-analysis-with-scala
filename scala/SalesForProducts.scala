972f268c2700f305843e7bd80bf27d503c101b9c
Ayabenta <75723449+Ayabenta@users.noreply.github.com>
2021-03-01 10:21:19 +0100

    Add files via upload

---
+++ scala/SalesForProducts.scala
@@ -0,0 +1,37 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+
+object SalesForProducts{
+  SparkSession.clearActiveSession()
+  private val SD = SparkSession
+    .builder()
+    .master("local[1]")
+    .appName("SData")
+    .config("spark.some.config.option", "some-value")
+    .getOrCreate()
+  val Sales = "Sales.txt"
+  val DataStruct = StructType(Array(
+    StructField("txID", IntegerType, false),
+    StructField("custID", IntegerType, false),
+    StructField("prodID", IntegerType, false),
+    StructField("timestamp", StringType, false),
+    StructField("amount", IntegerType, false),
+    StructField("quantity", IntegerType, false)
+  ))
+
+  val SalesData = SD.read
+    .option("sep", "|")
+    .schema(DataStruct)
+    .csv (Sales)
+
+  def main(args:Array[String]) = {
+    // val df = spark.createDataFrame(
+    //   spark.sparkContext.parallelize(simpleData),DataStruct)
+    SalesData.printSchema()
+    SalesData.show()
+    val Sales2013 = SalesData.filter(SalesData("timestamp").contains("2013"))
+    Sales2013.show(false)
+
+
+  }
+}
